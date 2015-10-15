a56c2ef11dcbfff403d48430860d6e54ef393418
Daz Miller <dxzdaz@gmail.com>
2015-10-14 19:23:45 -0700

    contact

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<head>
+
+	<title>Home view</title>
+
+	<link rel="stylesheet" type="text/css" href="index.css">
+
+	<meta charset="UTF-8">
+
+</head>
+
+<body>
+
+<?php include 'header.php';?>
+
+	<h1>Rockit Bootcamp</h1><br>
+
+		<b>Address:</b> Fountainhead Parkway<br>
+
+		<b>City/State:</b> Phoenix, AZ<br>
+
+		<b>Phone:</b>123-456-7890<br>
+
+<?php include 'footer.php';?>
+
+</body>
+
+</html>
