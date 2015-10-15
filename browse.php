cd94265e734674ec1c708d648a20f4f5be81fef7
Daz Miller <dxzdaz@gmail.com>
2015-10-14 19:21:17 -0700

    browse :wq :wq

---
+++ browse.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<head>
+
+	<title>Browse view</title>
+
+	<link rel="stylesheet" type="text/css" href="index.css">
+
+	<meta charset="UTF-8">
+
+</head>
+
+<body>
+
+	<?php include 'header.php';?>
+
+	<h1>Browse Products</h1>
+
+	<aside>
+
+	<nav>
+
+		<a href="product.php?id=1">MacBook<br></a>
+
+		<a href="product.php?id=2">Ubuntu<br></a>
+
+		<a href="product.php?id=3">Windows<br></a>
+
+	</nav>
+
+	</aside>
+
+	<?php include 'footer.php';?>
+
+</body>
+
+</html>
